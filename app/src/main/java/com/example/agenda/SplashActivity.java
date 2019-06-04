30c5bcb00bcb753d0e9014329553cd5c1b3fbe8a
Ramón <ramondav.24@gmail.com>
2019-06-04 19:48:45 +0200

    SlashPantalla

---
+++ app/src/main/java/com/example/agenda/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int DURACION=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, DURACION);
+    }
+}
